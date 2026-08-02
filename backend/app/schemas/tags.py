cd745c15448dc34dbd74906d6324dad3fc9a01ff
minseoriii <flower6713@naver.com>
2026-08-02 20:08:01 +0900

    Revert "Merge pull request #5 from minseoriii/fix/server-func"
    This reverts commit d8dd084ee0b3f2f45f17c9ad5c00f63fdb72f64f, reversing
    changes made to ffb26d8d675f7168d83e356344d0b212907dd3d5.

--- backend/app/schemas/tags.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class TagsResponse(BaseModel):
-    core_values: list[str]
-    life_domains: list[str]
-    sensory_tags: list[str]
-    companion_types: list[str]
